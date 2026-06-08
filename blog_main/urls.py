aa79cc6c62598ccc417ac5b9dc144923d294bc10
Shivaji <shivaji.chakravorty5@gmail.com>
2026-06-08 23:12:06 +0530

    Setup URL links for each posts

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -21,9 +21,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from blogs import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
+    path('post/<slug:slug>/', blog_views.blogs, name='blogs'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -7,16 +7,21 @@ from blogs.models import Blog, Category
 # Create your views here.
 
 def posts_by_category(request, category_id):
+  # Fetch posts that belong to the category with the given id
   posts = Blog.objects.filter(category_id=category_id, status='Publish').order_by('-created_at')
-  category = get_object_or_404(Category, id=category_id)
-  # try:
-  #   category = get_object_or_404(Category, id=category_id)
-  # except:
-  #   return redirect('home')  # Redirect to 404 if category does not exist
+  # Get the category object to display its name. This will raise a 404 if the category doesn't exist.
+  category = get_object_or_404(Category, pk=category_id)
   context = {
     'posts': posts,
     'category': category,
   }
   return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+  post = get_object_or_404(Blog, slug=slug, status='Publish')
+  context = {
+    'post': post,
+  }
+  return render(request, 'blog_detail.html', context)
 
 # Need to create the above HTML template to display the posts by category. The template should loop through the 'posts' context variable and display the relevant information for each post, such as title, author, created_at, etc.
